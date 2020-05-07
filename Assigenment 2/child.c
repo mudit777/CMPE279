56326b07cb6cabad5af31ca89711333e21f03a07
Udit Marolia <55039593+mudit777@users.noreply.github.com>
2020-05-06 17:31:03 -0700

    Update child.c

--- Assigenment 2/child.c
+++ Assigenment 2/child.c
@@ -1,1 +1,25 @@
 // child file
+#include <unistd.h> 
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <stdlib.h> 
+#include <netinet/in.h> 
+#include <string.h> 
+#define PORT 80
+int main(int argc, char const *argv[]) 
+{ 
+   int new_socket, valread; 
+    char buffer[1024] = {0}; 
+    char *hello = "Hello from server";
+  new_socket = atoi(argv[1]);
+  
+  
+setuid(65534);
+    int check = getuid();
+    printf("the uid is: %d", check);
+    valread = read( new_socket , buffer, 1024); 
+    printf("%s\n",buffer ); 
+    send(new_socket , hello , strlen(hello) , 0 ); 
+    printf("Hello message sent\n");
+  return 0;
+}
